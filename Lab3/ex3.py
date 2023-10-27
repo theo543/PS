8e767da46f1a877f7124f12f4d40f2dc59f282a0
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-10-27 22:07:24 +0300

    ex3

--- Lab3/ex3.py
+++ Lab3/ex3.py
@@ -9,24 +9,24 @@ import numpy as np
 # 0, 2 rejected
 # 1 no
 # 3 yes
-# expected p = 1/2
+# p = 1/2 = 0.5
 def cases_with_second_is_f(counts):
-    no = counts[1]
-    yes = counts[3]
+    no = counts[0]
+    yes = counts[2]
     return yes / (no + yes)
 
 # 0 rejected
 # 1, 2 no
 # 3 yes
-# expected p = 1 / 3
+# p = 1 / 3 = 0.(3)
 def cases_with_at_least_one_f(counts):
-    no = counts[1] + counts[2]
-    yes = counts[3]
+    no = counts[0] + counts[1]
+    yes = counts[2]
     return yes / (no + yes)
 
 def sim(n):
-    cases = np.random.randint(1, 4 + 1, size=n)
-    counts = np.bincount(cases)
+    cases = np.random.randint(0, 3 + 1, size=n)
+    counts = np.bincount(cases, minlength=4)
     prob_1 = cases_with_second_is_f(counts)
     prob_2 = cases_with_at_least_one_f(counts)
     print(f"Result for case 1: {prob_1}")
