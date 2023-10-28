f4db70a742aef1cb5dba4bd7948828dae9babf62
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-10-28 10:50:24 +0300

    add options

--- Lab3/ex4.py
+++ Lab3/ex4.py
@@ -1,6 +1,7 @@
 import numpy as np
 from mpmath import mp, nstr
 from math import inf
+from sys import argv
 
 def exact_formula(n, k):
     # (n! / (k!(n-k)!) / 2^n
@@ -8,31 +9,46 @@ def exact_formula(n, k):
     # arbitrary precision number
     return (f(n) / (f(k) * f(n - k))) / mp.power(2, n)
 
-def sim(n, k):
-    assert(k <= n)
-
+def sim(n: int, k: int, binomial: bool):
     trues = 0
     times = 0
     def log(end):
         print(f"p = {trues}/{times} = {trues/times:.10f}", end=end)
     try:
         gen = np.random.default_rng()
-        bin_gen_size = 20000000
+        bin_gen_size = 20_000_000
         while True:
-            counts = gen.binomial(n, 0.5, bin_gen_size)
-            trues += np.count_nonzero(counts == k)
-            times += bin_gen_size
+            if binomial:
+                counts = gen.binomial(n, 0.5, bin_gen_size)
+                trues += np.count_nonzero(counts == k)
+                times += bin_gen_size
+            else:
+                flips = gen.integers(0, high=1, size=n, dtype=bool, endpoint=True)
+                if np.count_nonzero(flips) == k:
+                    trues += 1
+                times += 1
             log('\r')
     except KeyboardInterrupt:
         log('\n')
 
 if __name__ == "__main__":
-    n = int(input("n = "))
-    k = int(input("k = "))
-    print(f"n = {n}, k = {k}")
+    n:int
+    k:int
+    binomial:bool
+
+    try:
+        parse_arg3 = {'-f': False, '-b': True}
+        n = int(argv[1])
+        k = int(argv[2])
+        binomial = parse_arg3[argv[3]]
+        print(f"n = {n}, k = {k}")
+    except (IndexError, ValueError, KeyError):
+        print("Usage: py ex4.py <n> <k> <-f (simulate flips with random bools)| -b (use np.random.binomial)>")
+        exit(1)
+    assert(n >= k)
 
     exact_result = exact_formula(n, k)
     print(f"exact formula => {nstr(exact_result, min_fixed=-50, max_fixed=inf)}")
 
-    print("Simulation:")
-    sim(n, k)
+    print("Simulation: " + ("(using np.random.binomial)" if binomial else "(using random bools)"))
+    sim(n, k, binomial)
