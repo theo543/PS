8963530d60137875096b82fbc5539ca0613ffbd2
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-02 12:18:18 +0200

    Fix numpy 2d bit stuff

--- Lab5/ex2.py
+++ Lab5/ex2.py
@@ -11,28 +11,18 @@ def simulate(n: int, batches: int) -> int:
 
     gen = np.random.default_rng()
 
-    count = 0
-    bits = gen.integers(0, high=1, size=n * batches + 2, dtype=bool, endpoint=True)
-    bits[-1] = 0
-    bits[-2] = 0
-    next_bit = bits[1:-1]
-    next_next_bit = bits[2:]
-    three_repeats = np.copy(bits[:-2])
-    three_repeats &= next_bit
-    three_repeats &= next_next_bit
+    bits_padded = gen.integers(0, high=1, size=[n + 2, batches], dtype=bool, endpoint=True)
+    rows = n + 2
+    bits_padded[(rows-2):rows, 0:batches] = False
+    bits = bits_padded[0:(rows - 2), 0:batches]
+    next_bit = bits_padded[1:(rows - 1), 0:batches]
+    next_next_bit = bits_padded[2:rows, 0:batches]
+    all_three = bits.copy()
+    all_three &= next_bit
+    all_three &= next_next_bit
+    any_in_batch = np.bitwise_or.reduce(all_three, axis=0)
 
-    is_not_last_two = np.repeat(True, n)
-    is_not_last_two[-1] = 0
-    is_not_last_two[-2] = 0
-    is_not_last_two = is_not_last_two.repeat(batches)
-
-    three_repeats &= is_not_last_two
-
-    three_repeats_2d = three_repeats.reshape([batches, n], order='C')
-    any_three_repeats = np.bitwise_or.reduce(three_repeats_2d, axis=1)
-    count_any_three_repeats = np.count_nonzero(any_three_repeats)
-
-    return count_any_three_repeats
+    return np.count_nonzero(any_in_batch)
 
 if __name__ == "__main__":
     ap = argp.ArgumentParser()
