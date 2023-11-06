019735410634fa81988f6876a4dd3c5b58e5ed98
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-06 14:14:41 +0200

    Add formula using functools.cache

--- Lab5/ex2.py
+++ Lab5/ex2.py
@@ -2,6 +2,7 @@ import numpy as np
 from fractions import Fraction
 import argparse as argp
 from itertools import product
+from functools import cache
 
 def bruteforce(n: int) -> Fraction:
     def list_has_triple_ones(l) -> bool:
@@ -12,6 +13,18 @@ def bruteforce(n: int) -> Fraction:
     combinations = product([0, 1], repeat=n)
     comb_has_triple_ones = map(list_has_triple_ones, combinations)
     return Fraction(sum(comb_has_triple_ones), 2**n)
+
+# TODO: has O(n) space, can be O(1)
+@cache
+def formula(n: int) -> Fraction:
+    if n < 3: return Fraction(0)
+    if n == 3: return Fraction(1, 8)
+    # P(n) = P(n - 1) + (1 - P(n - 4)) / 16
+    # either there were already three ones, or there weren't, but it ended with 011, and the last digit was one
+    already_was_true = formula(n - 1)
+    could_become_true_now = (1 - formula(n - 4)) / 8
+    became_true = could_become_true_now / 2
+    return already_was_true + became_true
 
 def simulate(n: int, batches: int) -> Fraction:
 
@@ -29,9 +42,6 @@ def simulate(n: int, batches: int) -> Fraction:
     any_in_batch = np.bitwise_or.reduce(all_three, axis=0)
 
     return Fraction(np.count_nonzero(any_in_batch), batches)
-
-def formula(n: int) -> Fraction:
-    return Fraction(0)
 
 def print_frac(name: str, frac: Fraction):
     print(f"{name}: {frac} ~= {float(frac) * 100}%")
