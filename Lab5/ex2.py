5dddadb9c1d3d4cb7c6263bd9f37905ba80e20ed
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-06 14:12:11 +0200

    separate bruteforce

--- Lab5/ex2.py
+++ Lab5/ex2.py
@@ -1,24 +1,19 @@
 import numpy as np
 from fractions import Fraction
-
 import argparse as argp
+from itertools import product
 
-import itertools
+def bruteforce(n: int) -> Fraction:
+    def list_has_triple_ones(l) -> bool:
+        for i in range(0, len(l) - 2):
+            if l[i] and l[i + 1] and l[i + 2]:
+                return True
+        return False
+    combinations = product([0, 1], repeat=n)
+    comb_has_triple_ones = map(list_has_triple_ones, combinations)
+    return Fraction(sum(comb_has_triple_ones), 2**n)
 
-def list_has_3(l) -> bool:
-    # temporary for testing if simulation is right
-    # REPLACE LATER
-    for i in range(0, len(l) - 2):
-        if all(l[i:(i+3)]):
-            return True
-    return False
-
-def formula(n: int) -> Fraction:
-    lst = map(list_has_3, itertools.product([0, 1], repeat=n))
-    return Fraction(sum(lst), 2**n)
-
-def simulate(n: int, batches: int) -> int:
-    if n < 3: return 0
+def simulate(n: int, batches: int) -> Fraction:
 
     gen = np.random.default_rng()
 
@@ -33,18 +28,29 @@ def simulate(n: int, batches: int) -> int:
     all_three &= next_next_bit
     any_in_batch = np.bitwise_or.reduce(all_three, axis=0)
 
-    return np.count_nonzero(any_in_batch)
+    return Fraction(np.count_nonzero(any_in_batch), batches)
+
+def formula(n: int) -> Fraction:
+    return Fraction(0)
+
+def print_frac(name: str, frac: Fraction):
+    print(f"{name}: {frac} ~= {float(frac) * 100}%")
 
 if __name__ == "__main__":
     ap = argp.ArgumentParser()
     ap.add_argument("-n", default=3, type=int)
     ap.add_argument("-b", "--batches", default=10_000, type=int)
+    ap.add_argument("--always-bf", action='store_true')
     args = ap.parse_args()
+
+    BRUTEFORCE_LIMIT = 20
+    use_bruteforce = (args.n <= BRUTEFORCE_LIMIT) or (args.always_bf)
 
     print(f"Chance of {args.n} random bits containing 111:")
 
-    exact_frac = formula(args.n)
-    print(f"Formula: {exact_frac} ~= {float(exact_frac) * 100}%")
-
-    events = simulate(args.n, args.batches)
-    print(f"Simulation: {events} / {args.batches} ~= {(events / args.batches) * 100}%")
+    if use_bruteforce:
+        print_frac("Bruteforce", bruteforce(args.n))
+    else:
+        print(f"Bruteforce: (not using bruteforce, would be too slow for {args.n})")
+    print_frac("Formula", formula(args.n))
+    print_frac("Simulation", simulate(args.n, args.batches))
