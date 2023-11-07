6c863f0df7cfaa3599748b35a15279f3fec4d61b
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-07 20:56:37 +0200

    Show more detail in pie plot

--- Lab4/ex2.py
+++ Lab4/ex2.py
@@ -5,12 +5,16 @@ import argparse
 def sim(n, a, b):
     rand = np.random.uniform(0, 1, size=n)
     in_range = np.count_nonzero(((rand > a) & (rand < b)))
-    ratio = in_range / n
-    print(f"result = {in_range} / {n} = {ratio}")
+    smaller = np.count_nonzero(rand < a)
+    greater = np.count_nonzero(rand > b)
+    in_range = n - smaller - greater
+    print(f"result = {in_range} / {n} = {in_range / n}")
     print(f"expected = {b} - {a} = {b - a}")
     fig, ax = plt.subplots()
     range = f"[{a}, {b}]"
-    ax.pie([1 - ratio, ratio], labels=[f"x ~E {range}", f"x E {range}"], autopct=lambda val: f"{val/100:.5f}")
+    ax.pie([smaller / n, in_range / n, greater / n],
+           labels=[f"x < {a}", f"x E [{a}, {b}]", f"x > {b}"],
+           autopct=lambda val: f"{val/100:.5f}")
     plt.show(block=True,)
 
 if __name__ == "__main__":
