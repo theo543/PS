7a2565dd0b8489573dda60c90745b2232862ea70
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-09 12:33:57 +0200

    Generalize ex2, for HW

--- Lab5/ex2.py
+++ Lab5/ex2.py
@@ -3,42 +3,43 @@ from fractions import Fraction
 import argparse as argp
 from itertools import product
 
-def bruteforce(n: int) -> Fraction:
-    def list_has_triple_ones(l) -> bool:
-        for i in range(0, len(l) - 2):
-            if l[i] and l[i + 1] and l[i + 2]:
+def bruteforce(n: int, repeat_len: int) -> Fraction:
+    def list_has_repeat(l) -> bool:
+        for i in range(0, len(l) - repeat_len + 1):
+            if all(l[i:(i+repeat_len)]):
                 return True
         return False
     combinations = product([0, 1], repeat=n)
-    comb_has_triple_ones = map(list_has_triple_ones, combinations)
+    comb_has_triple_ones = map(list_has_repeat, combinations)
     return Fraction(sum(comb_has_triple_ones), 2**n)
 
-def formula(n: int) -> Fraction:
-    if n < 3: return Fraction(0)
-    probs = [Fraction(0), Fraction(0), Fraction(0), Fraction(1, 8)]
-    for _ in range(4, n + 1):
-        # P(n) = P(n - 1) + (1 - P(n - 4)) / 16
-        # either there were already three ones, or there weren't, but it ended with 011, and the last digit was one
-        already_was_true = probs[3]
-        could_become_true_now = (1 - probs[0]) / 8
+def formula(n: int, repeat_len: int) -> Fraction:
+    if n < repeat_len: return Fraction(0)
+    probs = [Fraction(0)] * repeat_len + [Fraction(1, 2) ** repeat_len]
+    for _ in range(repeat_len + 1, n + 1):
+        # P(n) = P(n - 1) + (1 - P(n - NUM_BITS_IN_REPEAT - 1)) * (1 / 2 ^ (NUM_BITS_IN_REPEAT + 1))
+        # either there was already a repeat
+        # or there wasn't, but the last digits were 01111...1, and then a 1 was added to the end
+        # that requires (NUM_BITS_IN_REPEAT + 1) digits to have the exact right values
+        already_was_true = probs[-1]
+        could_become_true_now = (1 - probs[0]) / (2 ** repeat_len)
         became_true = could_become_true_now / 2
-        probs = [probs[1], probs[2], probs[3], already_was_true + became_true]
-    return probs[3]
+        probs = probs[1:] + [already_was_true + became_true]
+    return probs[-1]
 
-def simulate(n: int, batches: int) -> Fraction:
+def simulate(n: int, batches: int, repeat_len: int) -> Fraction:
 
     gen = np.random.default_rng()
 
-    bits_padded = gen.integers(0, high=1, size=[n + 2, batches], dtype=bool, endpoint=True)
-    rows = n + 2
-    bits_padded[(rows-2):rows, 0:batches] = False
-    bits = bits_padded[0:(rows - 2), 0:batches]
-    next_bit = bits_padded[1:(rows - 1), 0:batches]
-    next_next_bit = bits_padded[2:rows, 0:batches]
-    all_three = bits.copy()
-    all_three &= next_bit
-    all_three &= next_next_bit
-    any_in_batch = np.bitwise_or.reduce(all_three, axis=0)
+    rows = n + repeat_len - 1
+    bits_padded = gen.integers(0, high=1, size=[rows, batches], dtype=bool, endpoint=True)
+    bits_padded[n:rows, 0:batches] = False
+    bits = bits_padded[0:n, 0:batches]
+    repeat_at_position = bits.copy()
+    for i in range(1, repeat_len):
+        next_bit = bits_padded[i:(n + i), 0:batches]
+        repeat_at_position &= next_bit
+    any_in_batch = np.bitwise_or.reduce(repeat_at_position, axis=0)
 
     return Fraction(np.count_nonzero(any_in_batch), batches)
 
@@ -47,19 +48,21 @@ def print_frac(name: str, frac: Fraction):
 
 if __name__ == "__main__":
     ap = argp.ArgumentParser()
-    ap.add_argument("-n", default=3, type=int)
-    ap.add_argument("-b", "--batches", default=10_000, type=int)
+    ap.add_argument("-n", default=0, type=int)
+    ap.add_argument("-rb", "--repeat-bits", default=4, type=int)
+    ap.add_argument("-b", "--sim-batches", default=10_000, type=int)
     ap.add_argument("--always-bf", action='store_true')
     args = ap.parse_args()
+    if args.n == 0: args.n = args.repeat_bits
 
     BRUTEFORCE_LIMIT = 20
     use_bruteforce = (args.n <= BRUTEFORCE_LIMIT) or (args.always_bf)
 
-    print(f"Chance of {args.n} random bits containing 111:")
+    print(f"Chance of {args.n} random bits containing {'1' * args.repeat_bits}:")
 
     if use_bruteforce:
-        print_frac("Bruteforce", bruteforce(args.n))
+        print_frac("Bruteforce", bruteforce(args.n, args.repeat_bits))
     else:
         print(f"Bruteforce: (not using bruteforce, would be too slow for {args.n})")
-    print_frac("Formula", formula(args.n))
-    print_frac("Simulation", simulate(args.n, args.batches))
+    print_frac("Formula", formula(args.n, args.repeat_bits))
+    print_frac("Simulation", simulate(args.n, args.sim_batches, args.repeat_bits))
